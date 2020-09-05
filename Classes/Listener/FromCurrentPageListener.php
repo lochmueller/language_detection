add4e7bf4419a0f4974a2a90d2dd253c5a378499
Tim Lochmüller <tim@fruit-lab.de>
2020-09-05 12:13:51 +0200

    Change event listeners from getIndpEnv to request object

--- Classes/Listener/FromCurrentPageListener.php
+++ Classes/Listener/FromCurrentPageListener.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace LD\LanguageDetection\Listener;
 
 use LD\LanguageDetection\Event\HandleLanguageDetection;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
 
 class FromCurrentPageListener
 {
     public function __invoke(HandleLanguageDetection $event): void
     {
-        $referer = (string)GeneralUtility::getIndpEnv('HTTP_REFERER');
-        $baseUri = rtrim((string)$event->getSite()->getBase(), '/');
+        $serverInformation = $event->getRequest()->getServerParams();
 
-        if ('' !== $referer && StringUtility::beginsWith($referer, $baseUri)) {
+        $referer = $serverInformation['HTTP_REFERER'] ?? '';
+        $baseUri = rtrim((string)$event->getSite()->getBase(), '/');
+        if ('' !== $referer && StringUtility::beginsWith((string)$referer, $baseUri)) {
             $event->disableLanguageDetection();
         }
     }
--- Classes/Listener/WorkspacePreviewListener.php
+++ Classes/Listener/WorkspacePreviewListener.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace LD\LanguageDetection\Listener;
 
 use LD\LanguageDetection\Event\HandleLanguageDetection;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class WorkspacePreviewListener
 {
     public function __invoke(HandleLanguageDetection $event): void
     {
-        if (null !== GeneralUtility::_GP('ADMCMD_prev') || null !== GeneralUtility::_GP('ADMCMD_previewWS')) {
+        $arguments = $event->getRequest()->getQueryParams();
+        $argumentsNames = array_keys($arguments);
+        if (\in_array('ADMCMD_prev', $argumentsNames) || \in_array('ADMCMD_previewWS', $argumentsNames)) {
             $event->disableLanguageDetection();
         }
     }
