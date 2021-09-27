7c161ebb32588ed7eb5038a3e6f2da1b42861111
Tim <tim@fruit-lab.de>
2021-09-27 23:15:34 +0200

    Add BotListenerTest.php and cleanups

--- Classes/Check/BotListener.php
+++ Classes/Check/BotListener.php
@@ -6,7 +6,6 @@ namespace LD\LanguageDetection\Check;
 
 use LD\LanguageDetection\Event\CheckLanguageDetection;
 use Psr\Http\Message\ServerRequestInterface;
-use TYPO3\CMS\Core\Site\Entity\Site;
 
 class BotListener
 {
@@ -14,19 +13,13 @@ class BotListener
 
     public function __invoke(CheckLanguageDetection $event): void
     {
-        if ($event->getSite() instanceof Site && $this->isBot($event->getRequest())) {
+        if ($this->isBot($event->getRequest())) {
             $event->disableLanguageDetection();
         }
     }
 
     protected function isBot(ServerRequestInterface $request): bool
     {
-        $userAgent = $request->getHeader('user-agent');
-
-        if (\is_array($userAgent) && !empty($userAgent)) {
-            $userAgent = array_shift($userAgent);
-        }
-
-        return \is_string($userAgent) && preg_match(self::BOT_PATTERN, $userAgent);
+        return preg_match(self::BOT_PATTERN, $request->getHeaderLine('user-agent'));
     }
 }
---
+++ Tests/Unit/Check/BotListenerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LD\LanguageDetection\Tests\Check;
+
+use LD\LanguageDetection\Check\BotListener;
+use LD\LanguageDetection\Event\CheckLanguageDetection;
+use LD\LanguageDetection\Tests\Unit\AbstractTest;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Site\Entity\SiteInterface;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class BotListenerTest extends AbstractTest
+{
+    /**
+     * @covers \LD\LanguageDetection\Check\BotListener
+     * @covers \LD\LanguageDetection\Event\CheckLanguageDetection
+     * @dataProvider data
+     */
+    public function testInvalidSiteObject(string $userAgent, bool $result): void
+    {
+        $site = $this->createMock(SiteInterface::class);
+
+        $request = new ServerRequest(null, null, 'php://input', ['user-agent' => $userAgent]);
+        $event = new CheckLanguageDetection($site, $request);
+
+        $botListener = new BotListener();
+        $botListener($event);
+
+        self::assertEquals($result, $event->isLanguageDetectionEnable());
+    }
+
+
+    /**
+     * @covers \LD\LanguageDetection\Check\BotListener
+     * @covers \LD\LanguageDetection\Event\CheckLanguageDetection
+     */
+    public function testWithoutUserAgend(): void
+    {
+        $site = $this->createMock(SiteInterface::class);
+
+        $request = new ServerRequest();
+        $event = new CheckLanguageDetection($site, $request);
+
+        $botListener = new BotListener();
+        $botListener($event);
+
+        self::assertTrue($event->isLanguageDetectionEnable());
+    }
+
+    /**
+     * @return array<int, array<string|bool>>
+     */
+    public function data(): array
+    {
+        return [
+            ['AdsBot-Google', false],
+            ['Firefox', true],
+            ['Chrome', true],
+            ['Yandex-12378', false],
+        ];
+    }
+}
