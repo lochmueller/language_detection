7599093f9015be89c69d696aa96dfda3e9f62daa
Tim <tim@fruit-lab.de>
2021-09-27 22:31:47 +0200

    covers

--- Tests/Unit/Event/BuildResponseTest.php
+++ Tests/Unit/Event/BuildResponseTest.php
@@ -4,15 +4,37 @@ declare(strict_types=1);
 
 namespace LD\LanguageDetection\Tests\Event;
 
+use LD\LanguageDetection\Event\BuildResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Site\Entity\SiteInterface;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
+
 /**
  * @internal
  * @coversNothing
  */
 class BuildResponseTest extends AbstractEventTest
 {
-    public function testDummy(): void
+    /**
+     * @covers \LD\LanguageDetection\Event\BuildResponse
+     */
+    public function testEventGetterAndSetter(): void
     {
-        // @todo
-        self::assertTrue(true);
+        $site = $this->createMock(SiteInterface::class);
+        $request = $this->createMock(ServerRequestInterface::class);
+        $language = $this->createMock(SiteLanguage::class);
+
+        $event = new BuildResponse($site, $request, $language);
+
+        self::assertNull($event->getResponse());
+        self::assertEquals($site, $event->getSite());
+        self::assertEquals($request, $event->getRequest());
+        self::assertEquals($language, $event->getSelectedLanguage());
+
+        $response = $this->createMock(ResponseInterface::class);
+
+        $event->setResponse($response);
+        self::assertEquals($response, $event->getResponse());
     }
 }
