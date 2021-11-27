a9ef24dccebb6246d6edbe0244a31e6db3f850ad
Tim <tim@fruit-lab.de>
2021-11-27 23:18:11 +0100

    Add 12.x to tests

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.4.0-8.1.99',
-            'typo3' => '10.4.0-11.5.99'
+            'typo3' => '10.4.0-12.0.99'
         ],
     ],
     'autoload' => [
