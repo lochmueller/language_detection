1e7648525d8a75d081da3a1115010079aad41d9d
Tim <tim@fruit-lab.de>
2021-11-27 22:19:17 +0100

    Add php 8.1 support and run tests for TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'hdnet.de',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.0.99',
+            'php' => '7.4.0-8.1.99',
             'typo3' => '10.4.0-11.5.99'
         ],
     ],
