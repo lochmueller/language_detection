ed26803e1d4031b2336a2eb763a55353a992a67f
Tim Lochmüller <tim@fruit-lab.de>
2024-10-17 20:13:50 +0200

    Prepare v13 release

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'hdnet.de',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.1.99',
-            'typo3' => '11.5.0-12.99.99'
+            'php' => '8.1.0-8.3.99',
+            'typo3' => '12.4.0-13.4.99'
         ],
     ],
     'autoload' => [
