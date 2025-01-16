dd05d97ef7b625108a4e41ca71f547c0436fa9c6
Gregor Hermens <gregor.hermens@a-mazing.de>
2025-01-16 15:57:19 +0100

    [TASK] Remove version switch

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 use Lochmueller\LanguageDetection\Service\LocaleCollectionSortService;
 use Lochmueller\LanguageDetection\Service\TcaLanguageSelection;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\HttpUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 $GLOBALS['SiteConfiguration']['site']['columns']['enableLanguageDetection'] = [
     'label' => 'LLL:EXT:language_detection/Resources/Private/Language/locallang.xlf:enable',
@@ -69,31 +66,14 @@ $GLOBALS['SiteConfiguration']['site']['columns']['addIpLocationToBrowserLanguage
     ],
 ];
 
-$version11Branch = VersionNumberUtility::convertVersionNumberToInteger(GeneralUtility::makeInstance(Typo3Version::class)->getBranch()) >= VersionNumberUtility::convertVersionNumberToInteger('11.2');
-
-if ($version11Branch) {
-    $GLOBALS['SiteConfiguration']['site']['columns']['fallbackDetectionLanguage'] = [
-        'label' => 'LLL:EXT:language_detection/Resources/Private/Language/locallang.xlf:fallback.detection.language',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'itemsProcFunc' => TcaLanguageSelection::class . '->get',
-        ],
-    ];
-} else {
-    $GLOBALS['SiteConfiguration']['site']['columns']['fallbackDetectionLanguage'] = [
-        'label' => 'LLL:EXT:language_detection/Resources/Private/Language/locallang.xlf:fallback.detection.language',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'special' => 'languages',
-            'items' => [
-                ['', ''],
-            ],
-            'default' => '',
-        ],
-    ];
-}
+$GLOBALS['SiteConfiguration']['site']['columns']['fallbackDetectionLanguage'] = [
+    'label' => 'LLL:EXT:language_detection/Resources/Private/Language/locallang.xlf:fallback.detection.language',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectSingle',
+        'itemsProcFunc' => TcaLanguageSelection::class . '->get',
+    ],
+];
 
 $GLOBALS['SiteConfiguration']['site']['columns']['allowAllPaths'] = [
     'label' => 'LLL:EXT:language_detection/Resources/Private/Language/locallang.xlf:allow.all.paths',
