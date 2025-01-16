68211cd9427ee95ac569cfe6acab37c0cc24fcbb
Tim Lochmüller <tim@fruit-lab.de>
2025-01-16 20:42:55 +0100

    Increase version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'hdnet.de',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
+            'php' => '8.2.0-8.3.99',
             'typo3' => '12.4.0-13.4.99'
         ],
     ],
